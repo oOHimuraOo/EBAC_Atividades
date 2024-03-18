04973ab2880d6e8edac03472453f41a18cb7dea2
Felipe Cerqueira <felipecer06@gmail.com>
2024-03-18 19:19:38 -0300

    Otimização

--- HTML-JS-Exercicio/main.js
+++ HTML-JS-Exercicio/main.js
@@ -14,51 +14,39 @@ form.addEventListener('submit', function(e) {
     var valor_b = parseInt(campoB.value)
 
     var resultadoComparacao = comparadorValores(valor_a, valor_b)
-    
-    if (resultadoComparacao === 2) {
-        mensagemResultado = `O valor A informado foi: <b>${valor_a}</b><br/>O valor B informado foi: <b>${valor_b}</b><br/><br/>O valor <b>${valor_a}</b> é maior que o <b>${valor_b}</b>.<br/><br/> Os valores informados no Campo A e no Campo B estão invalidos.`
-        resultadoTexto.classList.remove('sucesso')
-        resultadoTexto.classList.add('fracasso')
+    console.log(resultadoComparacao)
+
+    if (resultadoComparacao === 1) {
+        mensagemResultado = `O valor A informado foi: <b>${valor_a}</b>.<br/>O valor B informado foi: <b>${valor_b}</b>.<br/><br/>O valor <b>${Math.max(valor_a,valor_b)}</b> é maior que o <b>${Math.min(valor_a,valor_b)}</b>.<br/><br/> Os valores informados no Campo A e no Campo B estão invalidos.`
     }
     else if (resultadoComparacao === -1){
-        mensagemResultado = `O valor A informado foi: <b>${valor_a}</b><br/>O valor B informado foi: <b>${valor_b}</b><br/><br/>O valor <b>${valor_b}</b> é maior que o <b>${valor_a}</b>.<br/><br/> Os valores informados no Campo A e no Campo B estão validos.`
-        resultadoTexto.classList.remove('fracasso')
-        resultadoTexto.classList.add('sucesso')
+        mensagemResultado = `O valor A informado foi: <b>${valor_a}</b>.<br/>O valor B informado foi: <b>${valor_b}</b>.<br/><br/>O valor <b>${Math.max(valor_a,valor_b)}</b> é maior que o <b>${Math.min(valor_a,valor_b)}</b>.<br/><br/> Os valores informados no Campo A e no Campo B estão validos.`
     }
-    else if (resultadoComparacao === 1){
-        mensagemResultado = `O valor A informado foi: <b>${valor_a}</b><br/>O valor B informado foi: <b>${valor_b}</b><br/><br/>O valor <b>${valor_b}</b> é maior que o <b>${valor_a}</b>.<br/><br/> Os valores informados no Campo A e no Campo B estão invalidos.`
-        resultadoTexto.classList.remove('sucesso')
-        resultadoTexto.classList.add('fracasso')
+    else {
+        mensagemResultado = `O valor A informado foi: <b>${valor_a}</b>.<br/>O valor B informado foi: <b>${valor_b}</b>.<br/><br/>Os valores são iguais.<br/><br/> Os valores informados no Campo A e no Campo B estão invalidos.`
     }
-    else if (resultadoComparacao === -2){
-        mensagemResultado = `O valor A informado foi: <b>${valor_a}</b><br/>O valor B informado foi: <b>${valor_b}</b><br/><br/>O valor <b>${valor_a}</b> é maior que o <b>${valor_b}</b>.<br/><br/> Os valores informados no Campo A e no Campo B estão validos.`
+
+    resultadoTexto.innerHTML = mensagemResultado
+
+    if (resultadoComparacao === -1) {
         resultadoTexto.classList.remove('fracasso')
         resultadoTexto.classList.add('sucesso')
     }
     else {
-        mensagemResultado = `O valor A informado foi: <b>${valor_a}</b><br/>O valor B informado foi: <b>${valor_b}</b><br/><br/>Os valores são iguais.<br/><br/> Os valores informados no Campo A e no Campo B estão invalidos.`
-        resultadoTexto.classList.remove('sucesso')
         resultadoTexto.classList.add('fracasso')
     }
-        
-    resultadoTexto.innerHTML = mensagemResultado
+
     resultadoTexto.style.display = 'block'
 })
 
 function comparadorValores(val1,val2){
-    if (val1 > val2 && val1 >= 0) {
-        return 2
-    }
-    else if (val1 > val2){
+    if (val1 > val2) {
         return 1
     }
-    else if (val1 < val2 && val2 >= 0){
-        return -1
-    }
-    else if (val1 < val2){
-        return -2
+    else if (val1 === val2){
+        return 0
     }
     else {
-        return 0
+        return -1
     }
 }
--- changelog.md
+++ changelog.md
@@ -104,3 +104,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - main.css
 - changelog.md
 
+## [0.0.8] - 2024-03-18
+### Modified
+- index.html
+- main.js -> otimização do codigo. Retirando redundancias e simplificando operações.
+- changelog.md
