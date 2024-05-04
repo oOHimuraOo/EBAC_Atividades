2a53545a01de81d896f8baad366f338b20958bfb
Felipe Cerqueira <felipecer06@gmail.com>
2024-05-04 18:23:14 -0300

    exerecio finalizado

--- CSS-Responsivo-Exercicio/JavaScript/main.js
+++ CSS-Responsivo-Exercicio/JavaScript/main.js
@@ -2,6 +2,8 @@ var modoEscuro = false
 var modoLupa = false
 
 $(document).ready(function(){
+    inicializar_estilo()
+
     $(".owl-carousel").owlCarousel({
         items: 1,
         loop: true,
@@ -15,8 +17,10 @@ $(document).ready(function(){
     })
     
     $('header').hover(function(){
-        $('.menuPrincipal nav').slideToggle()
-    })
+        if (verificarTamanhoTela()) {
+            $('.menuPrincipal nav').slideToggle()
+        }
+    })    
     
     $('#modoLupa').click(function(){
         if (modoLupa == false) {
@@ -43,7 +47,51 @@ $(document).ready(function(){
             ativarModoEscuro(modoEscuro)
         }
     })
+
+    $('.sobre').click(function(){
+        CarregarNovoMain()
+    })
+
+    $('.home').click(function(){
+        carregarPaginaInicial()
+    })
+
+
 })
+
+$(window).on('resize', function(){
+    atualizarVisibilidadeMenu()
+})
+
+function inicializar_estilo(){
+    var estilo_main = $('#estilo-principal')
+    var estilo_escuro = $('#estilo-escuro')
+    estilo_main.prop('disabled', false)
+    estilo_escuro.prop('disabled', true)
+}
+
+function atualizarVisibilidadeMenu(){
+    if (verificarTamanhoTela()){
+        $('.menuPrincipal nav').hide()
+    }
+    else {
+        $('.menuPrincipal nav').show()
+    }
+}
+
+function verificarTamanhoTela(){
+    var screenWidth = screen.width
+    var windowWidth = window.innerWidth
+    if (screenWidth > 1023) {
+        if (windowWidth > 1023) {
+            return true
+        }
+        else{
+            return false
+        }
+    }
+    return false
+}
 
 $('.botaoFlutuante > button').click(function(){
     $('html, body').animate({scrollTop : 0},800)
@@ -66,18 +114,38 @@ function ativarHoverZoom(Ativo=false){
     if (Ativo == true){
         $('.produtosLista p').addClass('ativar-hover-produtos-lista')
         $('.preco').addClass('ativar-hover-preco')
+        $('main > section p').addClass('ativar-hover-section-p')
     }
     else{
         $('.produtosLista p').removeClass('ativar-hover-produtos-lista')
         $('.preco').removeClass('ativar-hover-preco')
+        $('main > section p').removeClass('ativar-hover-section-p')
     }
 }
 
 function ativarModoEscuro(Ativo=false){
-    if (Ativo == true){
-        console.log('mudei as cores')
+    var estilo_main = $('#estilo-principal')
+    var estilo_escuro = $('#estilo-escuro')
+    if (Ativo){
+        estilo_main.prop('disabled', true)
+        estilo_escuro.prop('disabled', false)
     }
-    else {
-        console.log('coloquei as cores originais')
+    else{
+        estilo_main.prop('disabled', false)
+        estilo_escuro.prop('disabled', true)
     }
+}
+
+function CarregarNovoMain(){
+    $('main').load('./HTML/sobre.html main > section'), function(response, status, xhr){
+        if (status == "error"){
+            alert("ocorreu um erro ao carregar o conteudo. tente novamente mais tarde.")
+        }
+    }
+    $('main').css("min-height", "0")
+    $('main').css("padding-bottom", "50px")
+}
+
+function carregarPaginaInicial(){
+    location.reload()
 }
